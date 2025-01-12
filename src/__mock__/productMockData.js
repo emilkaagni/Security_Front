cf55e32a7f1950ded6ba31ee803df34e434f9963
Emil Basnyat <emilbasnyat@Emils-MacBook-Pro.local>
2025-01-12 08:39:36 +0545

    mock data

---
+++ src/__mock__/productMockData.js
@@ -0,0 +1,68 @@
+const productMockData = [
+    {
+        "_id": "66b90f087cc03484ae989d77",
+        "productName": "blah blah",
+        "productPrice": 3824,
+        "productCategory": "gadgets",
+        "productDescription": "sncdslk",
+        "productImage": "1723404040087-problem_M-S copy.jpg",
+        "createdAt": "2024-08-11T19:17:14.958Z",
+        "createdBy": {
+            "_id": "6697fc8f55045fc0963c9e65",
+            "fname": "userchange",
+            "lname": "one"
+        },
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "66ba5f692a52e817273af2c8",
+        "productName": "vsdkln",
+        "productPrice": 9374,
+        "productCategory": "electronics",
+        "productDescription": " vssdlk",
+        "productImage": "1723490153529-Screenshot 2024-06-11 at 3.10.03â¯PM.png",
+        "createdAt": "2024-08-12T19:15:09.890Z",
+        "createdBy": {
+            "_id": "66838432ca340b9eb7861375",
+            "fname": "Admin",
+            "lname": "Name"
+        },
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "66ba6e4627ec42002601a85d",
+        "productName": "good",
+        "productPrice": 212,
+        "productCategory": "Tutor",
+        "productDescription": "totot",
+        "productImage": "1723493958511-Screenshot 2024-08-12 at 12.00.42â¯AM.png",
+        "createdAt": "2024-08-12T19:52:30.916Z",
+        "createdBy": {
+            "_id": "669aa38a4787459f2aecbbd3",
+            "fname": "user2",
+            "lname": "2"
+        },
+        "reviews": [],
+        "__v": 0
+    },
+    {
+        "_id": "66ba70cd27ec42002601a878",
+        "productName": "build",
+        "productPrice": 2435,
+        "productCategory": "buildere",
+        "productDescription": "cool",
+        "productImage": "1723494605643-guitar_lesson.jpg",
+        "createdAt": "2024-08-12T19:52:30.916Z",
+        "createdBy": {
+            "_id": "669aa38a4787459f2aecbbd3",
+            "fname": "user2",
+            "lname": "2"
+        },
+        "reviews": [],
+        "__v": 0
+    }
+
+]
+module.exports = productMockData;
